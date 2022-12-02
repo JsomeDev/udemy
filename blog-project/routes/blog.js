9b853eb128aaaa5f5deb028ea95caeebad5ee5f6
Somi Joo <wnthal1211@naver.com>
2022-12-02 23:54:42 +0900

    feat: upload

--- blog-project/routes/blog.js
+++ blog-project/routes/blog.js
@@ -36,4 +36,49 @@ router.post("/posts", async function (req, res) {
   res.redirect("/posts");
 });
 
+router.get('/posts/:id', async function(req,res) {
+    const query = `
+    SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts 
+    INNER JOIN authors on posts.author_id = authors.id
+    WHERE posts.id = ? 
+    `;
+    const [posts] = await db.query(query,[req.params.id]);
+
+    if (!posts || posts.length === 0) {
+        res.status(404).render('404');
+    }
+
+    const postData = {
+        ...posts[0],
+        date: posts[0].date.toISOString(), //machine-readable
+        humanReadableDate: posts[0].date.toLocaleDateString('en-US', {
+            weekday: 'long',
+            year:'numeric',
+            month:'long',
+            day:'numeric'
+        })
+    };
+
+    res.render('post-detail', {post: postData});
+})
+
+router.get('/posts/:id/edit', async function() {
+    const query = `
+    SELECT * FROM posts WHERE id = ?
+    `;
+    const [posts] = await db.query(query, [req.params.id]);
+   
+    if (!posts || posts.length === 0) {
+        res.status(404).render('404');
+    }
+
+    res.render('update-post', {post: posts[0]});
+});
+
+router.post('/posts/:id/eidt', function(req,res){
+    const query = `
+    UPDATE posts SET title = ?, summary = ?, body = ?
+    `
+});
+
 module.exports = router;
